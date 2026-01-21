0f3ef6450e66aeade891a5a7e38bd8a32e76efeb
tortuguitahack <104583107+tortuguitahack@users.noreply.github.com>
2026-01-21 02:47:11 -0400

    Add Stripe webhook handler in stripe.js

---
+++ api/webhooks/stripe.js
@@ -0,0 +1,5 @@
+// api/webhooks/stripe.js - Manejador específico de webhooks
+exports.handler = async (req, res) => {
+  // Este archivo es alternativo si prefieres separar la lógica
+  // Puedes mover todo el código del webhook aquí
+};
